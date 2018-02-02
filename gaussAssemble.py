0cc7b77bbb365b1590bb1002aafd64839ffcf431
tealex <35243448+tealex13@users.noreply.github.com>
2018-02-02 09:25:07 -0700

    bug fix
    Fixed Node indexing error

--- gaussAssemble.py
+++ gaussAssemble.py
@@ -26,61 +26,54 @@ def numCubeMembers(n):
 def func(x):
     return 1
 
-
-#generate Gauss points
-dim = 2
-
-gaussPoints = np.array([-1/np.sqrt(3),1/np.sqrt(3)])
-#gaussPointNode = np.array([-1/np.sqrt(3)])
-gaussWeights = np.array([1,1])
-endPoints = np.array([-1,1])
-gaussPointNodes = np.arange(len(gaussPoints))
-
-numGP = len(gaussPointNodes)
-stackSize = len(endPoints)*numGP**(dim-1)
-stackSize1 = numGP**(dim-np.arange(dim))*len(endPoints)**np.arange(dim)
-
-#tempNumNodes = numGP**dim+dim*(dim-1)*stackSize
-
-
-
-memberCount = numCubeMembers(dim) #The number of each members(...4D-vol,)
-tempNumNodes = int(np.sum(memberCount*numGP**np.arange(dim,0,-1)))
-
-gaussPointNodeArray = np.zeros([tempNumNodes,dim])
-#gaussWeightsArray = np.zeros([tempNumNodes,1])
-
-currentIndex = stackSize1[0] #Keep track of which cells we are filling
-gaussPointNodeArray[:currentIndex,:] = mas.cartesian(np.matlib.repmat(gaussPointNodes,dim,1))
-
-for i in range(1,dim): #Iterate through each dimension
+def parEleGPAssemble(dim, gaussPoints = [-1/np.sqrt(3),1/np.sqrt(3)], gaussWeights = [1,1] ):
+    #generate Gauss points
+#    dim = 2
     
-    temp = [gaussPointNodes.tolist()]*(dim-i)
-    temp.extend([np.arange(dim-1,dim+len(endPoints)-1).tolist()]*i)#Tack on node numbers for the endpoints
-    temp = mas.cartesian(np.array(temp)) #Create a matrix with a missing row
+    gaussPoints = np.array(gaussPoints)
+    gaussWeights = np.array(gaussWeights)
     
-    gaussPointNodeArray[currentIndex:currentIndex+stackSize1[i]]=temp #Record
-    currentIndex = currentIndex+stackSize1[i]
-#    print(currentIndex,numGP**dim+stackSize*dim*(i-1),"\n")
-    for j in range(dim-i): #iterate through each row
+    endPoints = np.array([-1,1])
+    gaussPointNodes = np.arange(len(gaussPoints))
+    
+    numGP = len(gaussPointNodes)
+    stackSize1 = numGP**(dim-np.arange(dim))*len(endPoints)**np.arange(dim)
+    
+    memberCount = numCubeMembers(dim) #The number of each members(...4D-vol,)
+    memberSize = numGP**np.arange(dim,0,-1) #The number nodes to itegrate across for each member
+    tempNumNodes = int(np.sum(memberCount*memberSize))
+    
+    gaussPointNodeArray = np.zeros([tempNumNodes,dim])
+    #gaussWeightsArray = np.zeros([tempNumNodes,1])
+    
+    currentIndex = stackSize1[0] #Keep track of which cells we are filling
+    gaussPointNodeArray[:currentIndex,:] = mas.cartesian(np.matlib.repmat(gaussPointNodes,dim,1))
+    
+    for i in range(1,dim): #Iterate through each dimension
         
-        for k in range(i): #iterate through each rearrangement
-#            print(i,j,k,"\n")
-#            print(k-(i+j),k-(i+j+1))
-            temp[:,[k-(i+j),k-(i+j+1)]] = temp[:,[k-(i+j+1),k-(i+j)]]
-            gaussPointNodeArray[currentIndex:currentIndex+stackSize1[i]]=temp
-            currentIndex = currentIndex+stackSize1[i]
-#            print(currentIndex,numGP**dim+stackSize*(1+k+j+dim*(i-1)),"\n")
-
-
-# Assemble the guass Point Array and Weight Array using gaussPointNodeArray to index
-gaussPointsArray = np.append(gaussPoints,endPoints)[gaussPointNodeArray.astype(int)]
-gaussWeightsArray = np.product(np.append(gaussWeights,np.ones([1,len(endPoints)])) 
-                               [gaussPointNodeArray.astype(int)],1)
-   
+        temp = [gaussPointNodes.tolist()]*(dim-i)
+        temp.extend([np.arange(numGP,numGP+len(endPoints)).tolist()]*i)#Tack on node numbers for the endpoints
+        temp = mas.cartesian(np.array(temp)) #Create a matrix with a missing row
+        
+        gaussPointNodeArray[currentIndex:currentIndex+stackSize1[i]]=temp #Record
+        currentIndex = currentIndex+stackSize1[i]
+        for j in range(dim-i): #iterate through each row
+            
+            for k in range(i): #iterate through each rearrangement
+    
+                temp[:,[k-(i+j),k-(i+j+1)]] = temp[:,[k-(i+j+1),k-(i+j)]]
+                gaussPointNodeArray[currentIndex:currentIndex+stackSize1[i]]=temp
+                currentIndex = currentIndex+stackSize1[i]
+    
+    # Assemble the guass Point Array and Weight Array using gaussPointNodeArray to index
+    gaussPointsArray = np.append(gaussPoints,endPoints)[gaussPointNodeArray.astype(int)]
+    gaussWeightsArray = np.product(np.append(gaussWeights,np.ones([1,len(endPoints)])) 
+                                   [gaussPointNodeArray.astype(int)],1)
+    return (gaussPointsArray,gaussWeightsArray,memberCount,memberSize)   
 
 #def guassIntegrate():
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
 #    memberCount = numCubeMembers(1)
 #    print(memberCount)
+    (a,b,c,d) = parEleGPAssemble(1)
